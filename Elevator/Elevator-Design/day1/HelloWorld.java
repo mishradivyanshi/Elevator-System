c3b3276dea496172f676666c9cdc25d688d7e7c4
Divyanshi Mishra <143250423+mishradivyanshi@users.noreply.github.com>
2026-02-19 01:23:10 +0530

    Delete Elevator/Elevator-Design/day1 directory

--- Elevator/Elevator-Design/day1/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-//Author: Maitreyee Joshi -- Exercise: 1. Setup - HelloWorld
-
-package day1;
-
-public class HelloWorld {
-    void main(String[] args) {
-        System.out.println("Hello, Maitreyee!");
-    }
-}
